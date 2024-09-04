912abd05325f6ac64dcf675f57a848dfba47e3ff
Md. Mobashirul Alam Seam <mobashirulalamseam@gmail.com>
2024-09-04 22:34:04 +0600

    Basic navbar and footer added

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import "./index.css";
+import { router } from "./routes/routes";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+    <StrictMode>
+        <div className="max-w-screen-xl mx-auto">
+            <RouterProvider router={router} />
+        </div>
+    </StrictMode>
+);
